342845e21bd4a2c913a2ea3f416aa8fa6fb5b131
Enrique Serrano <e.serranogar@gmail.com>
2023-02-01 11:44:04 +0100

    new modules of users created

---
+++ Window_creation.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+import os, sys
+
+
+
+class Windows(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title('ERP EIPSA')
+        self.iconbitmap('eipsa.ico')
+        self.geometry('650x400')
