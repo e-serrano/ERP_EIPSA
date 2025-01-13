45c3ae855141d20c9293f2377957c947d5a36ba9
Enrique Serrano <enrique-serrano@eipsa.es>
2025-01-13 08:56:57 +0100

    finishing function to compare data

--- TAGEdit_Commercial_Window.py
+++ TAGEdit_Commercial_Window.py
@@ -985,7 +985,7 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
         self.toolDeleteFilter.clicked.connect(self.delete_allFilters)
         self.toolShow.clicked.connect(self.show_columns)
         self.toolExpExcel.clicked.connect(self.exporttoexcel)
-        # self.toolCompare.clicked.connect(self.data_comparison)
+        self.toolCompare.clicked.connect(self.data_comparison)
         try:
             self.Numoffer_EditTags.returnPressed.connect(self.query_tags)
         except Exception as error:
@@ -3142,10 +3142,13 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
             df_generated.columns = df_generated.iloc[0]
 
             if self.variable == 'Caudal':
-                df_generated = df_generated.iloc[1:,:39]
-            
+                df_generated = df_generated.iloc[1:,:34]
             elif self.variable == 'Temperatura':
                 df_generated = df_generated.iloc[1:,:39]
+            elif self.variable == 'Nivel':
+                df_generated = df_generated.iloc[1:,:40]
+            elif self.variable == 'Otros':
+                df_generated = df_generated.iloc[1:,:15]
 
         output_path = asksaveasfilename(defaultextension=".xlsx", filetypes=[("Archivos de Excel", "*.xlsx")], title="Guardar archivo de Excel")
         if output_path:
@@ -3157,34 +3160,45 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
             print("No se seleccionó ningún archivo.")
             return
 
-        # Read Excel file with original data
+    # Read Excel file with original data
         if self.variable == 'Caudal':
-            df_original = pd.read_excel(comparison_file, usecols=range(39), skiprows=7, dtype=str, keep_default_na=False)
+            df_original = pd.read_excel(comparison_file, usecols=range(34), skiprows=7, dtype=str, keep_default_na=False, na_values=[])
         elif self.variable == 'Temperatura':
-            df_original = pd.read_excel(comparison_file, usecols=range(39), skiprows=7, dtype=str, keep_default_na=False)
+            df_original = pd.read_excel(comparison_file, usecols=range(39), skiprows=7, dtype=str, keep_default_na=False, na_values=[])
+        elif self.variable == 'Nivel':
+            df_original = pd.read_excel(comparison_file, usecols=range(40), skiprows=7, dtype=str, keep_default_na=False, na_values=[])
+        elif self.variable == 'Otros':
+            df_original = pd.read_excel(comparison_file, usecols=range(15), skiprows=7, dtype=str, keep_default_na=False, na_values=[])
 
         if df_generated.shape != df_original.shape:
             print("Los archivos tienen diferentes formas y no se pueden comparar.")
             return
 
-        # Open generated excel
+    # Open generated excel
         workbook = load_workbook(output_path)
         sheet = workbook.active
 
-        # Red fill for diferencesRelleno rojo para diferencias
+    # Red fill for diferencesRelleno rojo para diferencias
         fill_red = PatternFill(start_color="fa5151", end_color="fa5151", fill_type="solid")
 
-        # Sorting both dataframes by ID
+    # Sorting both dataframes by ID
         df_generated = df_generated.sort_values(by=df_generated.columns[0])
         df_original = df_original.sort_values(by=df_original.columns[0])
 
-        # Compare cell by cell
+    # Change date format on dataframe with original data
+        df_original['contractual_date'] = pd.to_datetime(df_original['contractual_date'], errors='coerce')
+        df_original['contractual_date'] = df_original['contractual_date'].dt.strftime('%d/%m/%Y')
+        df_original['amount'] = (df_original['amount'].astype(float))
+
+    # Change price format on dataframe with final data
+        df_generated['Precio (€)'] = (df_generated['Precio (€)'].str.replace('.', '').str.replace(',', '.').str.replace(' €', '').astype(float))
+        df_generated['ID'] = (df_generated['ID'].astype(str))
+
+    # Compare cell by cell
         for row in range(df_generated.shape[0]):
             for col in range(df_generated.shape[1]):
                 gen_value = df_generated.iloc[row, col]
                 comp_value = df_original.iloc[row, col]
-
-                print(gen_value, comp_value)
 
                 if pd.isna(gen_value) and pd.isna(comp_value):
                     continue  # Both cell are NaN, no differences
@@ -3195,7 +3209,7 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
                     excel_col = col + 1  # +1 to adjust to Excel index (1-based)
                     sheet.cell(row=excel_row, column=excel_col).fill = fill_red
 
-        # Save file with differences filled
+    # Save file with differences filled
         workbook.save(output_path)
 
 
