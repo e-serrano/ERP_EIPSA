59443d4dca957d972bc2649cc88208c99a3f445d
Enrique Serrano <enrique-serrano@eipsa.es>
2025-01-28 12:25:27 +0100

    changing query for pending supplier report

--- Purchasing_Reports_Menu.py
+++ Purchasing_Reports_Menu.py
@@ -347,7 +347,7 @@ class Ui_Purchasing_Reports_Menu(QtWidgets.QMainWindow):
             cur = conn.cursor()
         # execution of commands
             commands_pending_client = ("""SELECT suppliers."name", supplies."reference", supplies."description", supplier_ord_header."supplier_order_num",
-                                            TO_CHAR(supplier_ord_header."order_date",'DD/MM/YYYY'), ROUND(supplier_ord_detail."quantity"::numeric, 2), ROUND(supplier_ord_detail."pending"::numeric, 2), supplies."unit_value",
+                                            TO_CHAR(supplier_ord_header."order_date",'DD/MM/YYYY'), ROUND(supplier_ord_detail."quantity"::numeric, 2), ROUND(supplier_ord_detail."pending"::numeric, 2), supplier_ord_detail."unit_value",
                                             supplier_ord_detail."pending" * supplies."unit_value" AS subtotal
                                         FROM 
                                             purch_fact.supplier_ord_header AS supplier_ord_header
