5d8f5935f61205b1e174b6bcb5b87c65d1b284fb
Enrique Serrano <enrique-serrano@eipsa.es>
2025-02-20 16:39:11 +0100

    editing function to compare files

--- TAGEdit_Commercial_Window.py
+++ TAGEdit_Commercial_Window.py
@@ -3268,11 +3268,15 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
             elif self.variable == 'Otros':
                 df_generated = df_generated.iloc[1:,:15]
 
-        output_path = asksaveasfilename(defaultextension=".xlsx", filetypes=[("Archivos de Excel", "*.xlsx")], title="Guardar archivo de Excel")
+    # Sorting dataframe by ID
+        df_generated[df_generated.columns[0]] = df_generated[df_generated.columns[0]].astype(int)
+        df_generated = df_generated.sort_values(by=df_generated.columns[0], ascending=True)
+
+        output_path = asksaveasfilename(defaultextension=".xlsx", filetypes=[("Archivos de Excel", "*.xlsx")], title="Guardar Excel Comparativo")
         if output_path:
             df_generated.to_excel(output_path, index=False, header=True)
-        
-        comparison_file = askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+
+        comparison_file = askopenfilename(filetypes=[("Excel files", "*.xlsx")], title="Seleccionar Excel Importación")
 
         if not comparison_file:
             print("No se seleccionó ningún archivo.")
@@ -3288,9 +3292,9 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
         elif self.variable == 'Otros':
             df_original = pd.read_excel(comparison_file, usecols=range(15), skiprows=7, dtype=str, keep_default_na=False, na_values=[])
 
-        if df_generated.shape != df_original.shape:
-            print("Los archivos tienen diferentes formas y no se pueden comparar.")
-            return
+        # if df_generated.shape != df_original.shape:
+        #     print("Los archivos tienen diferentes formas y no se pueden comparar.")
+        #     return
 
     # Open generated excel
         workbook = load_workbook(output_path)
@@ -3299,9 +3303,9 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
     # Red fill for diferencesRelleno rojo para diferencias
         fill_red = PatternFill(start_color="fa5151", end_color="fa5151", fill_type="solid")
 
-    # Sorting both dataframes by ID
-        df_generated = df_generated.sort_values(by=df_generated.columns[0])
-        df_original = df_original.sort_values(by=df_original.columns[0])
+    # Sorting dataframe by ID
+        df_original[df_original.columns[0]] = df_original[df_original.columns[0]].astype(int)
+        df_original = df_original.sort_values(by=df_original.columns[0], ascending=True)
 
     # Change date format on dataframe with original data
         df_original['contractual_date'] = pd.to_datetime(df_original['contractual_date'], errors='coerce')
@@ -3310,7 +3314,8 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
 
     # Change price format on dataframe with final data
         df_generated['Precio (€)'] = (df_generated['Precio (€)'].str.replace('.', '').str.replace(',', '.').str.replace(' €', '').astype(float))
-        df_generated['ID'] = (df_generated['ID'].astype(str))
+        if 'Espesor Placa' in df_generated.columns:
+            df_generated['Espesor Placa'] = (df_generated['Espesor Placa'].str.replace(',', '.').astype(str))
 
     # Compare cell by cell
         for row in range(df_generated.shape[0]):
@@ -3329,6 +3334,16 @@ class Ui_EditTags_Commercial_Window(QtWidgets.QMainWindow):
 
     # Save file with differences filled
         workbook.save(output_path)
+
+        dlg = QtWidgets.QMessageBox()
+        new_icon = QtGui.QIcon()
+        new_icon.addPixmap(QtGui.QPixmap(os.path.abspath(os.path.join(basedir, "Resources/Iconos/icon.ico"))), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+        dlg.setWindowIcon(new_icon)
+        dlg.setWindowTitle("ERP EIPSA")
+        dlg.setText("Excel comparativo generado")
+        dlg.setIcon(QtWidgets.QMessageBox.Icon.Information)
+        dlg.exec()
+        del dlg, new_icon
 
 
 if __name__ == "__main__":
