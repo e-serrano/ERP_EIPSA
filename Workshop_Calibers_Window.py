b51b6c80226b8ebf7a9fba6a148c22ae6643d496
Enrique Serrano <enrique-serrano@eipsa.es>
2025-03-19 14:46:19 +0100

    setting date editable

--- Workshop_Calibers_Window.py
+++ Workshop_Calibers_Window.py
@@ -248,7 +248,7 @@ class EditableTableModel(QtSql.QSqlTableModel):
             Qt.ItemFlags: The flags for the specified item.
         """
         flags = super().flags(index)
-        if index.column() in [0,4]:
+        if index.column() in [0,3]:
             flags &= ~Qt.ItemFlag.ItemIsEditable
             return flags | Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled
         else:
