3d780e7c4b3a3f93d7e9bf6e5ce2e8b840267d09
Enrique Serrano <enrique-serrano@eipsa.es>
2025-09-19 11:14:26 +0200

    adding function to get base path

--- config.py
+++ config.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python
 from configparser import ConfigParser
+from pathlib import Path
+import sys
+
+# Base directory for the project (common folder for all PCs)
+if getattr(sys, 'frozen', False):
+    # If running as PyInstaller exe
+    BASEDIR = Path(sys.executable).parent
+else:
+    # If running as normal Python script
+    BASEDIR = Path(r"\\nas01\DATOS\Comunes\EIPSA-ERP")
 
 
+# Helper function to build paths
+def get_path(*subpaths):
+    """
+    Returns an absolute path by joining BASEDIR with subfolders/files.
+
+    Example:
+        get_path("Resources", "Iconos", "icon.ico")
+    """
+    return (BASEDIR.joinpath(*subpaths)).resolve()
+
+
+# Database configuration reader
 def config(filename=r"C:\Program Files\ERP EIPSA\database.ini", section='postgresql'):
     """
     Reads database configuration from an INI file and returns the configuration parameters as a dictionary.
 
-     Args:
+    Args:
         filename (str): Path to the INI file containing the database configuration.
         section (str): The section in the INI file to read configuration from. Default is 'postgresql'.
 
