2de85be98f309a8a688f9a77cbaac89149e3eb86
Enrique Serrano <enrique-serrano@eipsa.es>
2025-09-23 11:20:11 +0200

    setting txt method to shutdown instances

--- EIPSA-ERP.py
+++ EIPSA-ERP.py
@@ -3,19 +3,22 @@ from PyQt6 import QtWidgets
 from Login_Window import Ui_Login_Window
 import os
 from PyQt6 import QtCore
-import psycopg2
 import psutil
-from config import config
-from utils.Database_Manager import Database_Connection
 from utils.Show_Message import show_message
+from config import get_path
+# import psycopg2
+# from config import config
+# from utils.Database_Manager import Database_Connection
 
+
+shutdown_file = get_path("Resources", "Logging", "shutdown.txt")
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.check_shutdown_signal)
-        self.timer.start(5000)  # Verify every 1000 ms (1 second)
+        self.timer.start(3000)  # Verify every 3000 ms (3 second)
 
     def close_all_instances(self, executable_name):
         """Close all instances of executable selected."""
@@ -30,19 +33,31 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def check_shutdown_signal(self):
         """Check if the shutdown signal file indicates a closure."""
-        try:
-            with Database_Connection(config()) as conn:
-                cur = conn.cursor()
-                cur.execute("SELECT action FROM logging.erp_control WHERE id = TRUE")
-                row = cur.fetchone()
+        # try:
+        #     with Database_Connection(config()) as conn:
+        #         cur = conn.cursor()
+        #         cur.execute("SELECT action FROM logging.erp_control WHERE id = TRUE")
+        #         row = cur.fetchone()
 
-            if row and row[0].lower() == "shutdown":
-                self.close_all_instances("EIPSA-ERP.exe")
-                self.close()
-            else:
-                print('Estado normal (OK), aplicación sigue funcionando')
-        except (Exception, psycopg2.DatabaseError) as error:
-            show_message(f"Ocurrió un error en la base de datos:\n{error}", "critical")
+        #     if row and row[0].lower() == "shutdown":
+        #         self.close_all_instances("EIPSA-ERP.exe")
+        #         self.close()
+        #     else:
+        #         print('Estado normal (OK), aplicación sigue funcionando')
+        # except (Exception, psycopg2.DatabaseError) as error:
+        #     show_message(f"Ocurrió un error en la base de datos:\n{error}", "critical")
+
+        try:
+            if os.path.exists(shutdown_file):
+                with open(shutdown_file, 'r') as f:
+                    content = f.read().strip()  # Read the content of the file
+                    if content != "OK":
+                        print("Recibiendo comando de cierre...")
+                        self.close_all_instances("EIPSA-ERP.exe")
+                        self.close()
+                        sys.exit()  # Exit the application
+        except Exception as e:
+            print(f"Error al verificar el archivo de señal: {e}")
 
 if __name__ == "__main__":
     """
