4e135a70d9bbc7f95ccb1d05e33cfd582d0ab4a5
Enrique Serrano <enrique-serrano@eipsa.es>
2025-10-28 08:58:20 +0100

    changing headers when query all data

--- DocEdit_Window.py
+++ DocEdit_Window.py
@@ -959,7 +959,9 @@ class Ui_EditDoc_Window(QtWidgets.QMainWindow):
         self.tableEditDocs.horizontalHeader().sectionClicked.connect(self.on_view_horizontalHeader_sectionClicked)
 
         # Change all column names
-        headers = ["Nº Doc. EIPSA", "Nº Doc. Cliente", "Nº Pedido", "Título", "Tipo Doc.", "Crítico", "Nº Revisión", "Fecha Estado", "Estado", "Seguimiento", "Fecha Primera Rev", "Info/Review", "Doc. Resp.", "Días"]
+        headers = ["Nº Doc. EIPSA", "Nº Doc. Cliente", "Nº Pedido", "Título", "Tipo Doc.",
+                "Crítico", "Nº Revisión", "Fecha Estado", "Estado", "Seguimiento",
+                "Fecha Primera Rev", "Info/Review", "Doc. Resp.", "Días"]
         self.model.setAllColumnHeaders(headers)
 
         self.combo_itemtype = EditableComboBoxDelegate(self.tableEditDocs, sorted(['Aprobado','Comentado','Com. Mayores','Com. Menores','Eliminado','Enviado','HOLD', 'Información', 'Rechazado']))
@@ -1026,7 +1028,9 @@ class Ui_EditDoc_Window(QtWidgets.QMainWindow):
         self.tableEditDocs.horizontalHeader().sectionClicked.connect(self.on_view_horizontalHeader_sectionClicked)
 
         # Change all column names
-        headers = ["Nº Doc. EIPSA", "Nº Doc. Cliente", "Nº Pedido", "Título", "Tipo Doc.", "Crítico", "Nº Revisión", "Fecha Estado", "Estado", "Seguimiento"]
+        headers = ["Nº Doc. EIPSA", "Nº Doc. Cliente", "Nº Pedido", "Título", "Tipo Doc.",
+                "Crítico", "Nº Revisión", "Fecha Estado", "Estado", "Seguimiento",
+                "Fecha Primera Rev", "Info/Review", "Doc. Resp.", "Días"]
         self.model.setAllColumnHeaders(headers)
 
         self.combo_itemtype = EditableComboBoxDelegate(self.tableEditDocs, sorted(['Aprobado','Comentado','Com. Mayores','Com. Menores','Eliminado','Enviado','Rechazado']))
