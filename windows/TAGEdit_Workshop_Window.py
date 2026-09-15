8da6f8a29c195eb0932811d4f869f97252b245a7
Enrique Serrano <enrique-serrano@eipsa.es>
2026-09-15 11:02:53 +0200

    editing export function

--- windows/TAGEdit_Workshop_Window.py
+++ windows/TAGEdit_Workshop_Window.py
@@ -2116,6 +2116,12 @@ class Ui_EditTags_Workshop_Window(QtWidgets.QMainWindow):
                     value = self.proxy.data(self.proxy.index(row, column))
                     if isinstance(value, QDate):
                         value = value.toString("dd/MM/yyyy")
+                    elif column in [50] and self.variable == 'Caudal':
+                        value = int(value) if value != '' else 0
+                    elif column in [15,16,60,61,62,63] and self.variable == 'Temperatura':
+                        value = int(float(value)) if value != '' else 0
+                    elif column in [17,18,19,20,21] and self.variable == 'Temperatura':
+                        value = float(value.replace(',', '.')) if value not in ('', 'N/A', 'HOLD') else 0
                     tag_data.append(value)
                 final_data.append(tag_data)
 
@@ -2129,6 +2135,38 @@ class Ui_EditTags_Workshop_Window(QtWidgets.QMainWindow):
                 if not output_path.lower().endswith(".xlsx"):
                     output_path += ".xlsx"
                 df.to_excel(output_path, index=False, header=True)
+
+            if self.variable2:
+                final_data = []
+
+                visible_columns = [col for col in range(self.model2.columnCount()) if not self.tableEditTags2.isColumnHidden(col)]
+                visible_headers = self.model2.getColumnHeaders(visible_columns)
+                original_headers = [self.model2.record().fieldName(col) for col in visible_columns]
+                for row in range(self.proxy2.rowCount()):
+                    tag_data = []
+                    for column in visible_columns:
+                        value = self.proxy2.data(self.proxy2.index(row, column))
+                        if isinstance(value, QDate):
+                            value = value.toString("dd/MM/yyyy")
+                        elif column in [15,16,60,61,62,63] and self.variable2 == 'Temperatura':
+                            value = int(float(value)) if value != '' else 0
+                        elif column in [17,18,19,20,21] and self.variable2 == 'Temperatura':
+                            value = float(value.replace(',', '.')) if value not in ('', 'N/A', 'HOLD') else 0
+                        tag_data.append(value)
+                    final_data.append(tag_data)
+
+                final_data.insert(0, visible_headers)
+                final_data.insert(1, original_headers)
+                df = pd.DataFrame(final_data)
+                df.columns = df.iloc[0]
+                df = df[1:]
+
+                output_path, _ = QtWidgets.QFileDialog.getSaveFileName(None, "Guardar Excel", "", "Archivos de Excel (*.xlsx)")
+                if output_path:
+                    if not output_path.lower().endswith(".xlsx"):
+                        output_path += ".xlsx"
+                    df.to_excel(output_path, index=False, header=True)
+
 
 # Function to enable copy and paste cells
     def keyPressEvent(self, event):
